733894badf69eea4801d7ac3bb82a1d70bfb901d
Faresii <105324937+Faresii@users.noreply.github.com>
2023-04-01 01:29:44 +0300

    Update main.cpp

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,17 +1,56 @@
+#include <iostream>
 #include "header.h"
+#include "class.cpp"
+#define LEN 80
 
+using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "rus");
-	Republic r1("Россия", 17098246, "какая-то партия");
+	char gosudarstvo[LEN];
+	int a = 0;
+	char country[LEN];
+
+	while(1){
+	cout << "ВЫБЕРЕТЕ ГОСУДАРСВТО : 1) РЕСПУБЛИКА 2) КОРОЛЕВСТВО 3) МОНАРХИЯ " << endl;
+	cin >> a;
+
+	if(a == 1){
+	cout << "Введите страну" << endl;
+	cin >> country;
+
+	Republic r1(country, 17098);
 	r1.Print();
-	Kingdom k1("UK", 242495, "Charlse III");
+	//break;
+	}
+
+	if(a == 2)
+		{
+	cout << "\nВведите страну" << endl;
+	cin >> country;
+
+	Kingdom k1(country, 242495);
 	k1.Print();
+	//break;
+		}
+
+	if(a == 3)
+	{
+	cout << "\nВведите страну" << endl;
+	cin >> country;
 	Monarchy* m1 = new Monarchy;
-	m1->SetArea(3500000);
-	m1->SetName("Византийсакя Империя");
-	m1->SetDynasty("Палеологи");
+	m1->SetArea(35000);
+	m1->SetName(country);
 	m1->Print();
 	delete m1;
+	//break; // освобождение динамической памяти 
+	}
+
+	if (a > 3 || a < 1) {
+		cout << "НЕКОРРЕКТНЫЙ ВВОД" << endl;
+		return 0;
+	}
+	}
+    system("pause");
 	return 0;
 }
